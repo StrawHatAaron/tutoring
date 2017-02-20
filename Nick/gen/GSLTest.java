cd368c89bf06abd29b4fc34b9cdd97ae74d21da1
aaronmiller <aaronmiller822028@gmail.com>
2017-02-19 21:58:47 -0800

    Improvements to generics — Time to do some web dev(:

---
+++ Nick/gen/GSLTest.java
@@ -0,0 +1,148 @@
+/*
+ * Code to test Generic Integer List
+ * 
+ * author Tim McGowen
+ * version 1.0
+ */
+public class GSLTest
+{
+   public static void main(String[] args)
+   {
+      int errors = 0;
+      GenericList<String> list = new GenericList<String>();
+      //GSL list = new GSL();
+      //System.out.println(list.size());
+      /******************************************************************/
+      /* A new list should return a size of zero                        */
+      /******************************************************************/
+      if ( list.size() != 0)
+      {
+         System.out.println("ERROR: A new list does not return size of 0");
+         errors++;
+      }
+   
+      /******************************************************************/
+      /* Adding an element should change the size to 1                  */
+      /******************************************************************/
+      list.add("3");
+      if ( list.size() != 1)
+      {
+         System.out.println("ERROR: Add one element to list does not return size of 1");
+         errors++;
+      }
+   
+      /******************************************************************/
+      /* Is value at the first position the value we expect?            */
+      /******************************************************************/
+      //System.out.println(list.get(0));
+      //System.out.println(list.get(1));
+      if ( list.get(0) != "3" )
+      {
+         System.out.println("ERROR: The value of the first element is not what we set it to be");
+         errors++;
+      }
+   
+      /******************************************************************/
+      /* Display the list                                               */
+      /******************************************************************/
+      System.out.println("Display list with one element");
+      list.display();
+   
+      /******************************************************************/
+      /* Add 9 more values to list. The size should be 10.              */
+      /******************************************************************/
+      for (int i = 0; i < 9; i++)
+         list.add("10");
+      if ( list.size() != 10 )
+      {
+         System.out.println("ERROR: The size is not equal to 10");
+         errors++;
+      }
+      // Display the list
+      System.out.println("Display list with ten elements");
+      list.display();
+   
+      /******************************************************************/
+      /* Add one more value. The list should expand                     */
+      /******************************************************************/
+      list.add("11");
+      if ( list.size() != 11)
+      {
+         System.out.println("ERROR: Add one more element to list does not return size of 11");
+         errors++;
+      }
+      if ( list.get(2) != "10")
+      {
+         System.out.println("ERROR: The value of the 11th element is not what we set it to be");
+         errors++;
+      }
+      System.out.println("Display list with eleven elements. List expanded");
+      list.display();
+   
+      /******************************************************************/
+      /* Insert a new element at the beginning of the list              */
+      /******************************************************************/
+      list.insert(0, "4");
+      if ( list.size() != 12)
+      {
+         System.out.println("ERROR: Add one more element to list does not return size of 12");
+         errors++;
+      }
+      if ( list.get(0) != "4" )
+      {
+         System.out.println("ERROR: The value of the first element is not what we set it to be");
+         errors++;
+      }
+      // Display the list
+      System.out.println("Display list with insert at index 5");
+      list.display();
+   
+      /******************************************************************/
+      /*Insert a new element in the middle of the list                  */
+      /******************************************************************/
+      list.insert(5, "3");
+      if ( list.size() != 13)
+      {
+         System.out.println("ERROR: Add one more element to list does not return size of 13");
+         errors++;
+      }
+      if ( list.get(5) != "3" )
+      {
+         System.out.println("ERROR: The value of the sixth element is not what we set it to be");
+         errors++;
+      }
+      // Display the list
+      System.out.println("Display list with insert at index 5");
+      list.display();
+      
+      /******************************************************************/
+      /* Display list as a string                                       */
+      /******************************************************************/
+      String stringList = list.toString();
+      if (!("4, 3, 10, 10, 10, 3, 10, 10, 10, 10, 10, 10, 11".equals(stringList)))
+      {
+         System.out.println("ERROR: toString does not return a properly formatted string");
+         System.out.println("ERROR: >" + stringList + "<");
+         System.out.println(stringList);
+         errors++;
+      }
+   
+      /******************************************************************/
+      /* Clear the list                                                 */
+      /******************************************************************/
+      list.clear();
+      // An empty list should return a size of zero
+      if ( list.size() != 0)
+      {
+         System.out.println("ERROR: An empty list does not return size of 0");
+         errors++;
+      }
+      // Display the list
+      System.out.println("Display list that has been cleared");
+      list.display();
+   
+      // What is the final result?
+      if ( errors == 0 )
+         System.out.println("No errors found!!!");
+   }
+}
